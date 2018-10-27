427a18c8ab5a6616cccc6fec45007fa67c8f839b
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-27 18:55:38 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -15,6 +15,6 @@ class AppointmentsController < ApplicationController
   
   private 
     def appointment_params
-      params.require(:doctor).permit(:name, :department)
+      params.require(:appointment).permit(:name, :department)
     end
 end
