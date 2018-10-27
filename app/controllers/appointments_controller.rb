6f3b07142e420dec8d4c8b1bca90522c969251ac
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-27 18:54:28 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,18 @@ class AppointmentsController < ApplicationController
   def show
     @appointment = Appointment.find(params[:id])
   end
+  
+  def new
+    @doctor = Doctor.new
+  end
+  
+  def create 
+    doctor = Doctor.create(doctor_params)
+    redirect_to doctor
+  end
+  
+  private 
+    def doctor_params
+      params.require(:doctor).permit(:name, :department)
+    end
 end
